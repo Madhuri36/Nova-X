5d5fa675319957487e92f4bd6e79409474ecf1e6
Sai Madhuri Bodapati <s.madhuri2k23@gmail.com>
2025-10-03 12:07:42 +0530

    added player

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,17 +2,18 @@ import { OrbitControls } from "@react-three/drei";
 import { Physics } from "@react-three/rapier";
 import Lights from "./Lights.jsx";
 import { Level } from "./Level/Level.jsx";
+import Player from "./Player";
 
 export default function Experience() {
   return (
     <>
       {/* Camera controls */}
       <OrbitControls makeDefault />
-
       {/* Rapier physics world */}
       <Physics>
         <Lights />
         <Level />
+        <Player />
       </Physics>
     </>
   );
