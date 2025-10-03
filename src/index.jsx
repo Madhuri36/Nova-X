291e8c1afccf8713d91287a4e65134d6f7f3fdd9
Musfiraa <musfiraaarif12@gmail.com>
2025-10-03 09:54:26 +0530

    Bugs Fixed

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
-
+import { KeyboardControls } from "@react-three/drei";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
@@ -20,7 +20,6 @@ root.render(
       shadows
       camera={{ fov: 45, near: 0.1, far: 200, position: [2.5, 4, 6] }}
     >
-   
       <Experience />
     </Canvas>
   </KeyboardControls>
